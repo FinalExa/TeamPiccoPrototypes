06f2cc8652219d1321734edd9eea260e02b89e06
Joe Abiad <Joenielscrs@gmail.com>
2022-02-03 11:51:58 +0100

    TestLevel

--- Assets/Scripts/TestLevel-Wall/TestLevelWall.cs
+++ Assets/Scripts/TestLevel-Wall/TestLevelWall.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class TestLevelWall : MonoBehaviour
 {
-    public GameObject roomToActive, room2ToDeactive, triggetToActive;
+    public GameObject roomToActive, room2ToDeactive, triggetToActive, Image1, Image2;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             roomToActive.SetActive(true);
+            Image1.SetActive(true);
             room2ToDeactive.SetActive(false);
+            Image2.SetActive(false);
             triggetToActive.SetActive(true);
             this.gameObject.SetActive(false);
         }
-
     }
 }
