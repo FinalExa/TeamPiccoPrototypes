7a79124d4623652bb47ad104d689cf99e30ae94c
Alex Silvestri <alexsilvestri.16@gmail.com>
2022-02-06 15:19:06 +0100

    Ai behaviour fixed in both maps

--- Assets/Scripts/Enemies/EnemyPattern.cs
+++ Assets/Scripts/Enemies/EnemyPattern.cs
@@ -61,7 +61,7 @@ public class EnemyPattern : MonoBehaviour
 
     private bool CheckOcclusionWithPlayer()
     {
-        Vector3 startPos = new Vector3(this.transform.position.x, this.transform.position.y + 1f, this.transform.position.z);
+        Vector3 startPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
         Vector3 direction = (playerRef.transform.position - startPos).normalized;
         bool canSeePlayer = false;
         if (Physics.Raycast(startPos, direction, out RaycastHit hit, Mathf.Infinity))
